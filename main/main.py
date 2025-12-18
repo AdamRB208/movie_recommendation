6cf1ab59a7eb3961a8b4a0f5b16b22259d5a3712
AdamRB208 <adamrborup@gmail.com>
2025-12-18 13:57:22 -0700

    added some move data.

--- main/main.py
+++ main/main.py
@@ -1,4 +1,13 @@
 
 MOVIES = [
-  
+    {"title": "Inception", "genre": "Sci-Fi", "rating": 8.8},
+    {"title": "The Dark Knight", "genre": "Action", "rating": 9.0},
+    {"title": "Titanic", "genre": "Romance", "rating": 7.8},
+    {"title": "The Godfather", "genre": "Crime", "rating": 9.2},
+    {"title": "Avatar", "genre": "Sci-Fi", "rating": 7.8},
+    {"title": "Forrest Gump", "genre": "Drama", "rating": 8.8},
+    {"title": "", "genre": "", "rating": ""},
+    {"title": "", "genre": "", "rating": ""},
+    {"title": "", "genre": "", "rating": ""},
+    {"title": "", "genre": "", "rating": ""},
 ]
