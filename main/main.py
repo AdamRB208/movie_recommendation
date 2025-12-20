816323e548e28ce76674aeaf872baecf84f88468
AdamRB208 <adamrborup@gmail.com>
2025-12-20 10:35:19 -0700

    added more movie data

--- main/main.py
+++ main/main.py
@@ -6,8 +6,9 @@ MOVIES = [
     {"title": "The Godfather", "genre": "Crime", "rating": 9.2},
     {"title": "Avatar", "genre": "Sci-Fi", "rating": 7.8},
     {"title": "Forrest Gump", "genre": "Drama", "rating": 8.8},
-    {"title": "", "genre": "", "rating": ""},
-    {"title": "", "genre": "", "rating": ""},
-    {"title": "", "genre": "", "rating": ""},
-    {"title": "", "genre": "", "rating": ""},
+    {"title": "Interstellar", "genre": "Sci-Fi", "rating": 9.0},
+    {"title": "The Martian", "genre": "Sci-Fi", "rating": 9.5 },
+    {"title": "Kingdom of Heaven", "genre": "Action", "rating": 8.5},
+    {"title": "Old School", "genre": "Comedy", "rating": 8.0},
 ]
+
